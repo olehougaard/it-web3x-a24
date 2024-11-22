ff6c32213f75d16f8520f805ce7020af83453f56
Ole Ildsgaard Hougaard <oih@via.dk>
2024-11-22 08:11:32 +0100

    initial commit

---
+++ 11 Next JS/tic-tac-toe - api - full/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { GamesList } from "@/components/GamesList";
+import { CreateGame } from "@/components/CreateGame";
+import * as api from "@/lib/api"
+
+export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+  const games = await api.readGamesList()
+  return (
+    <div>
+      <h2>Lobby</h2>
+      <h3>Available Games</h3>
+      <GamesList games={games}/>
+      <CreateGame/>
+    </div>
+  )
+}
